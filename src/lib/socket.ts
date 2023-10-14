a8cf0bf5a0d86c4d3dd3173dbf4d6e43ca7a3e83
Shouvik Mohanta <connectshouvik@gmail.com>
2023-10-14 13:36:58 +0530

    ip updated

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,7 +2,7 @@ import io, { Socket } from 'socket.io-client';
 // import { SOCKET_URL } from "config";
 
 // export const socket: Socket = io('ws://localhost:3001');
-export const socket: Socket = io('http://15.206.164.39:80');
+export const socket: Socket = io('http://15.207.114.45:80');
 // export const socket: Socket = io(
 //   `ws://${process.env.SOCKET_IP}:${process.env.SOCKET_PORT}`
 // );
