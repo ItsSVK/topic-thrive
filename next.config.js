30e937198dc3fd8e4e7c1ec1f3bc0226faa14374
Shouvik Mohanta <connectshouvik@gmail.com>
2023-10-18 10:10:32 +0530

    env updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-import webpack from 'webpack';
-import dotenv from 'dotenv';
-const { parsed: myEnv } = dotenv.config();
-const nextConfig = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,11 @@
 // config.ts
 
-export const PUSHER_APP_ID: string = process.env.PUSHER_APP_ID || '';
-export const PUSHER_APP_KEY: string = process.env.PUSHER_APP_KEY || '';
-export const PUSHER_APP_SECRET: string = process.env.PUSHER_APP_SECRET || '';
-export const PUSHER_APP_CLUSTER: string = process.env.PUSHER_APP_CLUSTER || '';
+export const PUSHER_APP_ID: string =
+  process.env.NEXT_PUBLIC_PUSHER_APP_ID || '';
+export const PUSHER_APP_KEY: string =
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY || '';
+export const PUSHER_APP_SECRET: string =
+  process.env.NEXT_PUBLIC_PUSHER_APP_SECRET || '';
+export const PUSHER_APP_CLUSTER: string =
+  process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER || '';
 // Add other environment variables as needed
