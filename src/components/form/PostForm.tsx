6d34c79d61c85e67b13fbb5bee4d1fe9a1e6b03b
Shouvik Mohanta <connectshouvik@gmail.com>
2023-11-14 20:39:14 +0530

    bug fix

--- src/components/form/PostForm.tsx
+++ src/components/form/PostForm.tsx
@@ -57,7 +57,7 @@ const PostForm: React.FC<PostFormProps> = ({
         <Button
           className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-3 lg:mt-0 w-44"
           type="submit"
-          disabled={isLoadingPostTopic ?? !allowPost}
+          disabled={isLoadingPostTopic || !allowPost}
         >
           {isLoadingPostTopic ? 'Please wait ...' : 'Post It'}
         </Button>
